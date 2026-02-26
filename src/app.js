5f8fb458ca3cb964b12dacc1100264c260f4d4cf
Mohit <mohitpandey9315339061@gmail.com>
2026-02-26 15:22:14 +0530

    feat: add cookie-parser and cors middleware for enhanced request handling

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import exprees from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+
+const app = exprees();
+
+app.use(cors({
+    origin: process.env.CLIENT_URL || "http://localhost:3000",
+    credentials: true
+}));
+app.use(exprees.json({limit: "16kb"}));
+app.use(exprees.urlencoded({extended: true, limit: "16kb"}));
+app.use(exprees.static("public"));
+app.use(cookieParser());
+
+
+ export default app;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import app from "./app.js";
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
 
@@ -5,4 +6,19 @@ dotenv.config({
     path: "./.env"
 });
 
-connectDB();
+connectDB()
+.then(
+    () => {
+        app.on("error", (error) => {
+            console.error("Error starting the server:", error);
+            process.exit(1); // Exit the process with an error code
+        });
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running on port ${process.env.PORT || 8000}`);
+        });
+    }
+)
+.catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1); // Exit the process with an error code
+});
